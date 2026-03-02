cfa80751743823e349bb35570c8a8d64c678dae1
Regnar110 <mateuszwrycza3@gmail.com>
2026-03-02 22:49:17 +0100

    Added baseUrl to tsconfig, removed root dir

--- org/packages/components/src/lib/components.tsx
+++ org/packages/components/src/lib/components.tsx
@@ -4,7 +4,7 @@ import styles from './components.module.css';
 export function MyorgComponents() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to MyorgComponents23s!</h1>
+      <h1>Welcome to MyorgComponents!</h1>
     </div>
   );
 }
